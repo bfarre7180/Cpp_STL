88c52a30e1d8742fe9b71be28642628ab9f79f6f
breaking bean <ferreb2@gmail.com>
2026-08-08 14:19:16 -0700

    moved the sequence container files to my_lib folder that way I can link it with the files better with less ambiguity. Got some tests running as well. Skeleton project is 90% done

--- containers/sequence_containers/tests/vec.cc
+++ containers/sequence_containers/tests/vec.cc
@@ -1,5 +1,25 @@
-#include "../include/vector"
+#include <my_lib/vector>
+#include <gtest/gtest.h>
 
-int main() {
-    my_lib::vector<int> vec{};
+TEST(VectorTest, Constructor_Sets_Size) {
+    my_lib::vector<int> vec1(5);
+    my_lib::vector<double> vec2{1.0, 2.0, 3.0, 4.0};
+
+    EXPECT_EQ(vec1.size(), 5);
+    EXPECT_EQ(vec2.size(), 4);
+}
+
+TEST(VectorTest, Element_Access) {
+    my_lib::vector<int> vec1(5);
+    my_lib::vector<double> vec2{1.0, 2.0, 3.0, 4.0};
+
+    vec1[0] = 10;
+    vec1[1] = 2;
+
+    vec2[0] = 99.9;
+
+    EXPECT_EQ(vec1[0], 10);
+    EXPECT_EQ(vec1[1], 2);
+    EXPECT_EQ(vec2[0], 99.9);
+    EXPECT_EQ(vec2[1], 2.0);
 }
